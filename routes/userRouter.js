6f28441c274d4706275ad8a3b098fbfbaeeb82d5
Etshawy1 <muhammad.mahmoud99@eng-st.cu.edu.eg>
2020-03-05 20:15:43 +0200

    test new approach swagger

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,6 +3,9 @@ const authController = require('../controllers/authController');
 const userController = require('../controllers/userController');
 
 const router = express.Router();
+
+
+
 /**
  * @swagger
  *
