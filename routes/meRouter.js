64bc8d3a02705595bdb01c57f0aad07451b4705e
MuhammedAlaa <soupermado@gmail.com>
2020-03-07 20:31:04 +0200

    add player to play track to loged in users

---
+++ routes/meRouter.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const authController = require('../controllers/authController');
+const meController = require('../controllers/meController');
+
+const router = express.Router();
+
+router.get('/player/tracks/:artist_id/:track_id', authController.protect, meController.playTrack);
+
+//thsi just for testing the player
+router.get('/gamed', function (req, res) {
+    res.sendFile(path.join(__dirname + '/../views/index.html'));
+});
+
+module.exports = router;
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -5,64 +5,6 @@ const userController = require('../controllers/userController');
 
 const router = express.Router();
 
-
-
-/**
- * @swagger
- *
- * definitions:
- *   NewUser:
- *     type: object
- *     required:
- *       - name
- *       - password
- *       - passwordConfirm
- *       - email
- *     properties:
- *       name:
- *         type: string
- *       email:
- *         type: string
- *         format: email
- *       password:
- *         type: string
- *         format: password
- *       passwordConfirm:
- *         type: string
- *         format: password
- *   User:
- *     allOf:
- *       - $ref: '#/definitions/NewUser'
- *       - required:
- *         - id
- *       - properties:
- *         id:
- *           type: integer
- *           format: int64
- */
-
-/**
- * @swagger
- * /v1/users/signup/:
- *  post:
- *    tags:
- *    - "signup"
- *    summary: "Add a new user to the application"
- *    consumes:
- *    - "application/json"
- *    produces:
- *    - "application/json"
- *    parameters:
- *      - name: NewUser
- *        schema:
- *           $ref: '#/definitions/NewUser'
- *    responses:
- *      201:
- *        description: "user created succefully"
- *      405:
- *        description: "Invalid input"
- */
-
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 router.get('/auth/google', passport.authenticate('google', {
