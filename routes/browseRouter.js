d42aa876274b537af7d27c5ebc212fca0f57f7cd
marait123 <44650320+marait123@users.noreply.github.com>
2020-03-23 08:49:28 -0700

    added the uploader utility for multipart forms

--- routes/browseRouter.js
+++ routes/browseRouter.js
@@ -2,41 +2,13 @@ const express = require('express');
 const authController = require('../controllers/authController');
 const browseController = require('../controllers/browseController');
 const path = require('path');
-const multer = require('multer');
-const imagePath = path.resolve(__dirname, '..') + '/assets/images/categories';
-const appError = require('../utils/appError');
-const imagePath1 = 'soso/';
-
-const fileFilter = (req, file, next) => {
-  // reject a file
-  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
-    next(null, true);
-  } else {
-    next(new appError("error file type is n't allowed", 400), false);
-  }
-};
-
-const storage = multer.diskStorage({
-  destination: function(req, file, cb) {
-    cb(null, imagePath);
-  },
-  filename: function(req, file, cb) {
-    console.log(file);
-    cb(null, file.originalname);
-  }
-});
-
-const upload = multer({
-  storage: storage,
-  fileFilter: fileFilter
-});
-
+const uploader = require('../utils/uploader');
 const router = express.Router();
 
 router.post(
   '/categories',
   authController.protect,
-  upload.array('icons', 12),
+  uploader.array('icons', 12),
   browseController.createCategory
 );
 
