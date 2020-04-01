ca62fb3bac8267dc4d30993deb9099f93ae4b057
Etshawy1 <muhammad.mahmoud99@eng-st.cu.edu.eg>
2020-04-01 15:13:03 +0200

    probable fix for password reset url problem

--- controllers/authController.js
+++ controllers/authController.js
@@ -147,9 +147,8 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   // 3) Send it to user's email
   try {
     const resetURL = `${req.protocol}://${req.hostname}/password-reset/change/${resetToken}`;
+    __logger.info(resetURL);
     await new Email(user, resetURL).sendPasswordReset();
-    __logger.info(req.hostname);
-    __logger.info(req.get('host'));
     res.status(200).json({
       status: 'success',
       message: 'Token sent to email!'
