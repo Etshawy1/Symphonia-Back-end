dbb00ef50706c143423cc770f90926105e5f9dfe
Etshawy1 <muhammad.mahmoud99@eng-st.cu.edu.eg>
2020-04-03 14:02:22 +0200

    adding sockets to the project

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,3 +1,14 @@
+const path = require('path');
+const io = require('socket.io')(80);
 module.exports = (req, res, next) => {
-  res.send('Hello worldaasdasdasd');
+  if (io.sockets._events == undefined) {
+    io.on('connection', socket => {
+      socket.emit('welcome', { hello: 'world' });
+      socket.on('my other event', function (data) {
+        console.log(data);
+      });
+    });
+  }
+
+  res.sendFile(path.join(__dirname, '../views/testSockets.html'));
 };
