4adc85e26afbfb980889008b055a9d383e141080
Etshawy1 <muhammad.mahmoud99@eng-st.cu.edu.eg>
2020-04-03 14:31:25 +0200

    modified socket.io

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,14 +1,5 @@
 const path = require('path');
 const io = require('socket.io')(80);
 module.exports = (req, res, next) => {
-  if (io.sockets._events == undefined) {
-    io.on('connection', socket => {
-      socket.emit('welcome', { hello: 'world' });
-      socket.on('my other event', function (data) {
-        console.log(data);
-      });
-    });
-  }
-
   res.sendFile(path.join(__dirname, '../views/testSockets.html'));
 };
