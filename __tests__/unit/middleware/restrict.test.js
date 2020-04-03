3a79d7ba1fe4034bcac7a43e7c91ba0610b53c8b
Etshawy1 <muhammad.mahmoud99@eng-st.cu.edu.eg>
2020-04-03 16:52:35 +0200

    migration replace role with type in user model

--- __tests__/unit/middleware/restrict.test.js
+++ __tests__/unit/middleware/restrict.test.js
@@ -3,14 +3,14 @@ const AppError = require('../../../utils/appError');
 
 describe('authorization middleware', () => {
   it('should call the next function if user is authorized', () => {
-    const req = { user: { role: 'user' } };
+    const req = { user: { type: 'user' } };
     const res = {};
     const next = jest.fn();
     restrictTo('admin', 'user')(req, res, next);
     expect(next).toHaveBeenCalledWith();
   });
   it('should return with message that user is unauthorized', () => {
-    const req = { user: { role: 'user' } };
+    const req = { user: { type: 'user' } };
     const res = {};
     const next = jest.fn();
     restrictTo('admin')(req, res, next);
--- controllers/authController.js
+++ controllers/authController.js
@@ -120,9 +120,9 @@ exports.protect = catchAsync(async (req, res, next) => {
   req.user = currentUser;
   next();
 });
-exports.restrictTo = (...roles) => {
+exports.restrictTo = (...types) => {
   return (req, res, next) => {
-    if (!roles.includes(req.user.role)) {
+    if (!types.includes(req.user.type)) {
       return next(
         new AppError('You do not have permission to perform this action', 403)
       );
