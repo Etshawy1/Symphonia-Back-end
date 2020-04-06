2396113cf942c0073990c1ee3b6cccfefd861a7c
Etshawy1 <muhammad.mahmoud99@eng-st.cu.edu.eg>
2020-04-06 13:24:08 +0200

    modify google Oauth for deployment

--- startup/passport-setup.js
+++ startup/passport-setup.js
@@ -15,7 +15,8 @@ passport.deserializeUser(function (user, done) {
 passport.use(
   new GoogleStrategy(
     {
-      callbackURL: 'http://localhost:3000/api/v1/users/auth/google/Symphonia',
+      callbackURL:
+        'http://zasymphonia.ddns.net/api/v1/users/auth/google/Symphonia',
       clientID: process.env.CLIENT_ID_GOOGLE,
       clientSecret: process.env.CLIENT_SECRET_GOOGLE
     },
