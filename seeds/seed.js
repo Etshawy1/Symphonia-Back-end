2962fef905cc836e16ebd63924b2099bcb3d111c
Etshawy1 <muhammad.mahmoud99@eng-st.cu.edu.eg>
2020-04-07 20:00:09 +0200

    populate some response instead of id only for the lazy androids

--- seeds/seed.js
+++ seeds/seed.js
@@ -7,10 +7,7 @@ require('dotenv').config();
 // but if you choose the online one the used npm library won't connect as online connection takes time
 // you would need to change value of (connectTimeoutMS) to be like 500 or so in
 // node_modules\mongo-seeding\dist\database\database-connector.js
-const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
-  process.env.DATABASE_PASSWORD
-);
+const DB = process.env.DATABASE;
 
 const config = {
   database: DB,
