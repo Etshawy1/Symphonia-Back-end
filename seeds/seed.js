5da96bfb26790314f0cbd667cac1c4baad3a38e2
Etshawy1 <muhammad.mahmoud99@eng-st.cu.edu.eg>
2020-04-08 10:41:45 +0200

    final touch in seed configuration file

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,13 +1,16 @@
 const path = require('path');
 process.env.DEBUG = 'mongo-seeding';
 const { Seeder } = require('mongo-seeding');
-require('dotenv').config('../.env');
+require('dotenv').config({ path: '../.env' });
 
 // the .env file in the root directory determines the database to be seeded
 // but if you choose the online one the used npm library won't connect as online connection takes time
 // you would need to change value of (connectTimeoutMS) to be like 500 or so in
-// node_modules\mongo-seeding\dist\database\database-connector.js
-const DB = process.env.DATABASE;
+// .\seeds\node_modules\mongo-seeding\dist\database\database-connector.js
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
 
 const config = {
   database: DB,
