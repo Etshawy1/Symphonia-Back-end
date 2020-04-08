454a020fa8ce7bee450d1243c481a3d02e779543
Etshawy1 <muhammad.mahmoud99@eng-st.cu.edu.eg>
2020-04-08 10:28:28 +0200

    forgot to save a file before previous commit

--- seeds/seed.js
+++ seeds/seed.js
@@ -3,7 +3,7 @@ process.env.DEBUG = 'mongo-seeding';
 const { Seeder } = require('mongo-seeding');
 require('dotenv').config('../.env');
 
-// you should have a .env file in this directory to connect to a database to be seeded
+// the .env file in the root directory determines the database to be seeded
 // but if you choose the online one the used npm library won't connect as online connection takes time
 // you would need to change value of (connectTimeoutMS) to be like 500 or so in
 // node_modules\mongo-seeding\dist\database\database-connector.js
